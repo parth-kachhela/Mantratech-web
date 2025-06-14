da5d904c229057db7b5ed32e2d8bbc2064957127
Parth Kachhela <parthkachhela78@gmail.com>
2025-06-14 11:42:00 +0530

    init

--- User/src/Pages/ContectUs.tsx
+++ User/src/Pages/ContectUs.tsx
@@ -1,5 +1,3 @@
-// ContactUsPage.tsx
-
 import { useState } from "react";
 import { Input } from "@/Components/ui/input";
 import { Textarea } from "@/Components/ui/textarea";
@@ -7,110 +5,7 @@ import { Button } from "@/Components/ui/button";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import { motion } from "framer-motion";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
-
-const mapOptions = {
-  styles: [
-    {
-      elementType: "geometry",
-      stylers: [{ color: "#1d2c4d" }],
-    },
-    {
-      elementType: "labels.text.fill",
-      stylers: [{ color: "#8ec3b9" }],
-    },
-    {
-      elementType: "labels.text.stroke",
-      stylers: [{ color: "#1a3646" }],
-    },
-    {
-      featureType: "administrative.country",
-      elementType: "geometry.stroke",
-      stylers: [{ color: "#4b6878" }],
-    },
-    {
-      featureType: "landscape.natural",
-      elementType: "geometry",
-      stylers: [{ color: "#023e58" }],
-    },
-    {
-      featureType: "poi",
-      elementType: "geometry",
-      stylers: [{ color: "#283d6a" }],
-    },
-    {
-      featureType: "road",
-      elementType: "geometry",
-      stylers: [{ color: "#304a7d" }],
-    },
-    {
-      featureType: "road",
-      elementType: "labels.icon",
-      stylers: [{ visibility: "off" }],
-    },
-    {
-      featureType: "transit",
-      elementType: "labels.text.fill",
-      stylers: [{ color: "#98a5be" }],
-    },
-    {
-      featureType: "water",
-      elementType: "geometry",
-      stylers: [{ color: "#0e1626" }],
-    },
-    {
-      featureType: "water",
-      elementType: "labels.text.fill",
-      stylers: [{ color: "#4e6d70" }],
-    },
-  ],
-};
-
-const locationData = [
-  {
-    city: "Rajkot",
-    position: { lat: 22.2733, lng: 70.7829 },
-  },
-  {
-    city: "Ahmedabad",
-    position: { lat: 23.0225, lng: 72.5714 },
-  },
-  {
-    city: "Bengaluru",
-    position: { lat: 12.9716, lng: 77.5946 },
-  },
-  {
-    city: "Veraval",
-    position: { lat: 20.9085, lng: 70.3606 },
-  },
-];
-
-const mapContainerStyle = {
-  width: "100%",
-  height: "100%",
-};
-
-const center = {
-  lat: 21.7679,
-  lng: 72.8716,
-};
-
-function OfficeMap() {
-  return (
-    <LoadScript googleMapsApiKey="AIzaSyC4fDaz9e1DokMCTru8ldQzwQCuHVBnxRA">
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        options={mapOptions}
-        center={center}
-        zoom={5}
-      >
-        {locationData.map((loc, idx) => (
-          <Marker key={idx} position={loc.position} title={loc.city} />
-        ))}
-      </GoogleMap>
-    </LoadScript>
-  );
-}
+import { OfficeMap } from "@/Components/OfficeMap";
 
 export default function ContactUsPage() {
   const [formData, setFormData] = useState({
