08c6cdb1fa571cc6cc387a4fadf53748a3e83a79
Parth Kachhela <parthkachhela78@gmail.com>
2025-06-15 18:17:37 +0530

    init

--- backend/src/Routes/ContectRoute.ts
+++ backend/src/Routes/ContectRoute.ts
@@ -1,31 +1,5 @@
-import { Router, Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
+// const router = Router();
+//
 
-const router = Router();
-const prisma = new PrismaClient();
-
-router.post("/contact", async (req: Request, res: Response) => {
-  const { firstName, lastName, address, mobile, email, other, enquiryType } =
-    req.body;
-
-  try {
-    const contact = await prisma.contact.create({
-      data: {
-        firstName,
-        lastName,
-        address,
-        mobile,
-        email,
-        other,
-        enquiryType,
-      },
-    });
-
-    res.status(200).json({ message: "Contact saved", contact });
-  } catch (error) {
-    console.error("Contact save error:", error);
-    res.status(500).json({ error: "Unable to save contact info" });
-  }
-});
-
-export default router;
+// router
+// export default router;
